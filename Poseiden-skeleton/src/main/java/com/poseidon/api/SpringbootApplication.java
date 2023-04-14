bc3587ce96936178e31ef8116374de6bd8fb30c6
ArtGane <argane.samson@gmail.com>
2023-04-14 07:55:36 +0200

    injecte deux users en base de données au démarrage de l'app

--- Poseiden-skeleton/src/main/java/com/poseidon/api/SpringbootApplication.java
+++ Poseiden-skeleton/src/main/java/com/poseidon/api/SpringbootApplication.java
@@ -1,12 +1,29 @@
 package com.poseidon.api;
 
+import com.poseidon.api.config.UserInjector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpringbootApplication {
+@ComponentScan(basePackageClasses = com.poseidon.api.config.AppConfig.class)
+public class SpringbootApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+    private UserInjector userInjector;
+
+    @Autowired
+    public SpringbootApplication(UserInjector userInjector) {
+        this.userInjector = userInjector;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        userInjector.init();
+    }
 }

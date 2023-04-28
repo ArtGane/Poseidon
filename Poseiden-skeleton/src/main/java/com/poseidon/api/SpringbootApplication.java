24f2bd6bd0f501ac25589a1baf187a898c498a44
ArtGane <argane.samson@gmail.com>
2023-04-28 08:53:40 +0200

    fin configuration oauth2

--- Poseiden-skeleton/src/main/java/com/poseidon/api/SpringbootApplication.java
+++ Poseiden-skeleton/src/main/java/com/poseidon/api/SpringbootApplication.java
@@ -1,29 +1,12 @@
 package com.poseidon.api;
 
-import com.poseidon.api.config.UserInjector;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = com.poseidon.api.config.AppConfig.class)
-public class SpringbootApplication implements CommandLineRunner {
-
-    private UserInjector userInjector;
-
-    @Autowired
-    public SpringbootApplication(UserInjector userInjector) {
-        this.userInjector = userInjector;
-    }
+public class SpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        userInjector.init();
     }
 }
--- Poseiden-skeleton/src/main/java/com/poseidon/api/config/SpringSecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/poseidon/api/config/SpringSecurityConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -30,11 +30,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/style/**").permitAll()
+                .antMatchers("/login").anonymous()
                 .antMatchers("/").anonymous()
                 .antMatchers("/user/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().defaultSuccessUrl("/bidList/list", true)
+                .and()
+                .oauth2Login().defaultSuccessUrl("/bidList/list", true)
                 .and()
                 .logout().logoutUrl("/app-logout").invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID")
